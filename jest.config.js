1641672e14dd882c60f848941bba278a277057bc
Ben Gao <xingbingao@gmail.com>
2019-11-11 14:53:33 +1300

    Init jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  collectCoverageFrom: ['app/**/*.js', '!app/**/*.test.js'],
+  collectCoverageFrom: ['src/**/*.js', '!src/**/*.test.js'],
   cacheDirectory: '<rootDir>/jest_cache',
   moduleNameMapper: {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '.+\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
   },
-  setupFilesAfterEnv: ['<rootDir>/testHelper/testSetup.js'],
-  testRegex: '__tests__/.*\\.test\\.js$'
+  setupFilesAfterEnv: ['<rootDir>/testHelper/jestSetup.js'],
+  testRegex: '__tests__/.*\\.test\\.js$',
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest'
+  }
 };
