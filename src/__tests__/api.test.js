ca8e95939d2cab1865ba39758e0c6e19bd9116fb
Ben Gao <xingbingao@gmail.com>
2019-11-20 13:33:51 +1300

    Add tests on transaction

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -1,18 +1,12 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
-// import { api } from '../api';
-import config from '../../config';
+import initApiPromise from '../../testHelper/initApiPromise';
+import { Keyring } from '@polkadot/keyring';
+
+const Alice = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+const BOB = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty';
 
 describe('api', () => {
   test('should be able to be initialized', async () => {
-    console.log('process.env.TEST_TYPE: ', process.env.TEST_TYPE);
-    console.log('config', config);
-
-    const providerUrl = config.wsProvider[`${process.env.TEST_TYPE || 'default'}`];
-    console.log('providerUrl', providerUrl);
-
-    const wsProvider = new WsProvider(providerUrl);
-    const api = await ApiPromise.create({ provider: wsProvider });
-
+    const api = await initApiPromise();
     await api.isReady;
 
     const [chain, nodeName, nodeVersion] = await Promise.all([
@@ -22,5 +16,42 @@ describe('api', () => {
     ]);
 
     console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+  });
+
+  describe('transfer', () => {
+    test('should be able to made', async () => {
+      const api = await initApiPromise();
+      await api.isReady;
+
+      const keyring = new Keyring({ type: 'sr25519' });
+      const alice = keyring.addFromUri('//Alice');
+
+      let previous = await api.query.balances.freeBalance(Alice);
+
+      console.log(`${Alice} has a balance of ${previous}`);
+      console.log(
+        `You may leave this example running and start example 06 or transfer any value to ${Alice}`
+      );
+
+      const transfer = api.tx.balances.transfer(BOB, 12345);
+      const hash = await transfer.signAndSend(alice);
+
+      console.log('Transfer sent with hash', hash.toHex());
+
+      // Here we subscribe to any balance changes and update the on-screen value
+      api.query.balances.freeBalance(Alice, current => {
+        // Calculate the delta
+        const change = current.sub(previous);
+
+        // Only display positive value changes (Since we are pulling `previous` above already,
+        // the initial balance change will also be zero)
+        if (!change.isZero()) {
+          previous = current;
+          console.log(`New balance change of: ${change}`);
+        }
+      });
+
+      expect(true);
+    });
   });
 });
